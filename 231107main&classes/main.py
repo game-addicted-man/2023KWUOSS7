b445c417aa513e0e680eaa9ffac84f3ed058d9aa
Juhwan Bae <73511160+PR5GRAMM2R@users.noreply.github.com>
2023-11-14 10:34:50 +0900

    디렉터리 생성 추가

--- 231107main&classes/main.py
+++ 231107main&classes/main.py
@@ -25,30 +25,30 @@ if __name__ == '__main__':
     cv2.destroyAllWindows()
 
     #이름 3번 인식 시 카메라로 사진을 찍어서 주민등록증의 사진 추출 후 face.jpg 파일로 저장
-    image = cv2.imread("./database/card.png")
+    image = cv2.imread(database_path + "/card.png")
     cropped_img = image[90:310, 420:600]
-    cv2.imwrite("./database/face.png",cropped_img)
+    cv2.imwrite(database_path + "face.png",cropped_img)
     #정보의 양을 늘리는 것(즉, 해상도를 늘리는 것)은 불가능하기에 필터링으로 이미지를 선명하게 변환
     improve1 = cv2.detailEnhance(cropped_img, sigma_s=10, sigma_r=0.15)
-    cv2.imwrite("./database/improved_face.png", improve1)
+    cv2.imwrite(database_path + "improved_face.png", improve1)
 
     #sharpening(선명하게)한 것, 제일 쓸만한듯
     kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]) 
     improve3 = cv2.filter2D(cropped_img, -1, kernel) 
-    cv2.imwrite('./database/sharpened_face.png', improve3)
+    cv2.imwrite(database_path + "sharpened_face.png", improve3)
 
-    mustBeRemovedFile = './database/*.pkl'  # image 폴더 내의 *.pkl 확장자 파일을 삭제해주어야 함.
+    mustBeRemovedFile = database_path + "*.pkl"  # image 폴더 내의 *.pkl 확장자 파일을 삭제해주어야 함.
 
     for f in glob(mustBeRemovedFile):
         os.remove(f)
 
-    fd = faceDetect.faceDetect("./database")
+    fd = faceDetect.faceDetect(database_path)
     # # fd.face_show("/Users/choejm/image/jaemin0.jpg")
     # print(fd.match_face(img1="image/MESSI.jpg"))
     # print(fd.match_face("image/jonhun.jpg", "image/jaemin0.jpg", name="일론머스크"))
     
     now_img = fd.current_face()
     print(fd.match_face(now_img))
-    print(fd.match_face("./database/sharpened_face.png", "captured.png", name=hi.name))
-    print(fd.match_face("./database/improved_face.png", "captured.png", name=hi.name))
-    print(fd.match_face("./database/face.png", "captured.png", name=hi.name))
+    print(fd.match_face(database_path + "sharpened_face.png", "captured.png", name=hi.name))
+    print(fd.match_face(database_path + "improved_face.png", "captured.png", name=hi.name))
+    print(fd.match_face(database_path + "face.png", "captured.png", name=hi.name))
